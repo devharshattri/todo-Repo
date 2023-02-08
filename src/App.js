642f9038ddff64315860f36e6ecbd198ac708cc8
devharshattri <devharsh1.attri@gmail.com>
2023-02-08 09:58:27 +0530

    Adding Async Await

--- src/App.js
+++ src/App.js
@@ -2,45 +2,46 @@ import axios from "axios";
 import React, { useState } from "react";
 import CardList from "./components/CardList";
 import "./App.css";
-// import { fetchData } from "./components/fetchData";
 
 function App() {
-  const [repo, setRepo] = useState('');
+  const [repo, setRepo] = useState("");
   const [list, setList] = useState([]);
   const [isloading, setLoading] = useState(false);
 
-  const getList = (e) => {
+  const getList = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setList([])
-    axios
-      .get(`https://api.github.com/search/repositories?q=${repo}`)
-      .then((res) => {
-        setLoading(false);
-        setList(res.data.items);
-        setRepo("");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  
+    setList([]);
+
+    try {
+      const res = await axios.get(
+        `https://api.github.com/search/repositories?q=${repo}`
+      );
+      setLoading(false);
+      setList(res.data.items);
+      setRepo("");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
-    <div >
+    <div>
       <div className="form-container">
         <form className="form" onSubmit={getList}>
           <input value={repo} onChange={(e) => setRepo(e.target.value)} />
-          <button className="form-btn" type="submit">Find Repo</button>
+          <button className="form-btn" type="submit">
+            Find Repo
+          </button>
         </form>
-      {isloading && <p>Loading...</p>}
-      {list.length !== 0  && (
-        <div >
-          <h2>Repos</h2>
-          <CardList list={list} />
-        </div>
-      )}
-      {list.length === 0 && <p>No Repo is available</p>}
+        {isloading && <p>Loading...</p>}
+        {list.length !== 0 && (
+          <div>
+            <h2>Repos</h2>
+            <CardList list={list} />
+          </div>
+        )}
+        {list.length === 0 && <p>No Repo is available</p>}
       </div>
     </div>
   );
--- src/components/fetchData.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export async function fetchData (repo) {
- const res  = await axios.get(
-    `https://api.github.com/search/repositories?q=${repo}`
-  );
-
-return res.data.items
-}
